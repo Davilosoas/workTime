d2c580d5f75bd11fb519c7b075d99fec618b0aa5
Davilosoas <davilosoas@gmail.com>
2025-02-16 00:59:25 -0300

    add migrations

--- backend/server.js
+++ backend/server.js
@@ -15,5 +15,16 @@ app.use(cors());
 app.use("/api/users", userRoutes);
 app.use("/api/activities", activityRoutes);
 
+app.get("/", (req, res) => {
+  res.send("🚀 Backend WorkTime está rodando! Acesse /api para usar a API.");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Servidor rodando na porta ${PORT}`));
+
+import { sequelize } from "./config/database.js";
+
+sequelize
+  .sync({ force: false })
+  .then(() => console.log("✅ Tabelas sincronizadas com o banco de dados!"))
+  .catch((err) => console.error("❌ Erro ao sincronizar tabelas:", err));
